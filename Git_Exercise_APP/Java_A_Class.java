5c079e848f1865d825ce6d69cce9e37d9c8863ee
BalazsiOliver <balazsi.oliver04@gmail.com>
2024-06-20 19:04:53 +0200

    a5

--- Git_Exercise_APP/Java_A_Class.java
+++ Git_Exercise_APP/Java_A_Class.java
@@ -5,8 +5,10 @@ public class Java_A_Class {
 
     public void count() {
          System.out.println(30);
+        System.out.println("I am fine :)");
     }
     public void print() {
        System.out.println("Hello People");
+        System.out.println("no im not");
     }
 }
